03e45d7b41391ca26ea55b1605622d1093f074fb
Hadrien Grasland <hadrien.grasland@ijclab.in2p3.fr>
2024-08-28 14:42:16 +0200

    Use str::contains where applicable

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1379,7 +1379,7 @@ pub(crate) mod tests {
 
         // Non-alternate displays should fit in a single line
         for non_alternate in [&display, &debug] {
-            prop_assert!(!non_alternate.chars().any(|c| c == '\n'));
+            prop_assert!(!non_alternate.contains('\n'));
         }
 
         // Debug output should be longer than or identical to Display output
@@ -1768,7 +1768,7 @@ pub(crate) mod tests {
                     let res = obj.set_subtype(&subtype);
 
                     // Handle inner NULs
-                    if subtype.chars().any(|c| c == '\0') {
+                    if subtype.contains('\0') {
                         prop_assert_eq!(res, Err(NulError));
                         return Ok(());
                     }
@@ -1788,7 +1788,7 @@ pub(crate) mod tests {
                     let res = obj.add_info(&name, &value);
 
                     // Handle inner NULs
-                    if name.chars().chain(value.chars()).any(|c| c == '\0') {
+                    if name.contains('\0') || value.contains('\0') {
                         prop_assert_eq!(res, Err(NulError.into()));
                         return Ok(());
                     }
